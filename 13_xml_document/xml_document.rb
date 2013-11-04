373d74a4f3594a8ed0b03a245de4eb7a1e401093
Will Hastings <will@kalamuna.com>
2013-11-03 18:40:14 -0800

    Almost solve xml document.

---
+++ 13_xml_document/xml_document.rb
@@ -0,0 +1,39 @@
+class XmlDocument
+
+  def initialize(indent = false)
+    @indent = indent
+    @indentation = 0
+    @tag = ""
+  end
+
+  def method_missing(method, *args, &block)
+puts @indentation.to_s + " " + method.to_s
+    tag = ""
+    starting_indentation = (@indentation - 2) > 0 ? 0 : 2
+    add(tag, "<#{method}", false, starting_indentation)
+    attributes = args[0]
+    if attributes.is_a?(Hash)
+      attributes.each { |name, value| tag << " #{name}='#{value}'" }
+    end
+    if block.nil?
+      tag << "/>"
+    else
+      tag << ">"
+      tag << "\n" if @indent
+      @indentation += 2 if @indent
+      result = block.call
+      @indentation -=2 if @indent
+      add(tag, result, true)
+      end_indentation = (@indentation - 2) > 0 ? (@indentation - 2) : 0
+      add(tag, "</#{method}>", false, end_indentation)
+    end
+    tag
+  end
+
+  def add(text, suffix, newline = false, indent = nil)
+    indentation = @indent ? indent || @indentation : 0
+    text << (" " * indentation) << suffix
+    text << "\n" if @indent && newline
+  end
+
+end
